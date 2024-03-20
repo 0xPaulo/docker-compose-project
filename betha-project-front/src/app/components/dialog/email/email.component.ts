10189ec1fdb3c7f3d7d1815807b6dfc5e29387c5
0xPaulo <paulo_czar@outlook.com.br>
2024-03-20 16:52:41 -0300

    add email send change status to waiting

--- betha-project-front/src/app/components/dialog/email/email.component.ts
+++ betha-project-front/src/app/components/dialog/email/email.component.ts
@@ -1,5 +1,10 @@
+import { DatePipe } from "@angular/common";
 import { Component, Inject } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { RepositoryService } from "src/app/services/repository.service";
+import { TabelaService } from "src/app/services/tabela.service";
 
 @Component({
   selector: "app-email",
@@ -7,5 +12,55 @@ import { MAT_DIALOG_DATA } from "@angular/material/dialog";
   styleUrls: ["./email.component.scss"],
 })
 export class EmailComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  form: FormGroup;
+  dia: string = "";
+
+  constructor(
+    private datePipe: DatePipe,
+    private tabelaService: TabelaService,
+    private repository: RepositoryService,
+    private snackBar: MatSnackBar,
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.form = formBuilder.group({
+      cliente: [data.infoCadastro.cliente],
+      endereco: [data.infoCadastro.endereco],
+      telefone: [data.infoCadastro.telefone],
+      email: [data.infoCadastro.email],
+      anotacao: [data.infoCadastro.anotacao],
+      item: [data.infoCadastro.item],
+      itemSerie: [data.infoCadastro.itemSerie],
+      status: [data.infoCadastro.status],
+      data_entrada: [data.infoCadastro.dataEntrada],
+      desc: [data.infoCadastro.desc],
+      image_urls: [data.infoCadastro.image_urls],
+      laudo: [data.infoCadastro.laudo],
+    });
+  }
+  handleSendEmail() {
+    const id = this.data.infoCadastro._id;
+    this.form.patchValue({ status: "AGUARDANDO_CLIENTE" });
+    const dataInicial = this.form.value.data_entrada;
+    const dataFormatada = this.datePipe.transform(
+      dataInicial,
+      "yyyy-MM-ddTHH:mm:ss"
+    );
+    const dadosAtualizados = { ...this.form.value, dataEntrada: dataFormatada };
+    this.repository.mudarStatus(id, dadosAtualizados).subscribe(
+      (result) => {
+        this.tabelaService.emitListaAtualizada.emit();
+        this.onSucess();
+      },
+      () => {
+        this.onError();
+      }
+    );
+  }
+  onError() {
+    this.snackBar.open("Acorreu um erro", "", { duration: 5000 });
+  }
+  onSucess() {
+    this.snackBar.open("Atualizado com sucesso", "", { duration: 5000 });
+  }
 }
