4326d8d98e1e8e3f3932b0152cedbbedc5ade51d
0xPaulo <paulo_czar@outlook.com.br>
2024-03-26 16:39:09 -0300

    novas entidades

---
+++ betha-project-back/src/main/java/com/betha/backend/cadastros/models/Cliente.java
@@ -0,0 +1,25 @@
+package com.betha.backend.cadastros.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cliente", schema = "manutencao")
+public class Cliente extends Pessoa {
+
+  @OneToMany(mappedBy = "cliente")
+  private List<Chamado> chamados = new ArrayList<>();
+
+}
