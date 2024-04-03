93657c49946db6cf8f0abf1b639743cd2f1b4294
0xPaulo <paulo_czar@outlook.com.br>
2024-04-03 07:16:28 -0300

    #

--- betha-project-front/src/app/screens/triagem/form-triagem/form-triagem.component.ts
+++ betha-project-front/src/app/screens/triagem/form-triagem/form-triagem.component.ts
@@ -67,7 +67,7 @@ export class FormTriagemComponent {
     );
   }
   onError() {
-    this.snackBar.open("Acorreu um erro", "", { duration: 5000 });
+    this.snackBar.open("Ocorreu um erro", "", { duration: 5000 });
   }
   onSucess() {
     this.snackBar.open("Atualizado com sucesso", "", { duration: 5000 });
