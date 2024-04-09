422ef8f70e0f90b2aec130147dbebc8c263c4558
0xPaulo <paulo_czar@outlook.com.br>
2024-04-09 19:55:32 -0300

    add create cliente on toolbar

--- betha-project-front/src/app/components/toolbar/toolbar.component.ts
+++ betha-project-front/src/app/components/toolbar/toolbar.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Router } from "@angular/router";
+import { DialogCriarClienteComponent } from "./dialog-criar-cliente/dialog-criar-cliente.component";
 
 @Component({
-  selector: 'betha-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss'],
+  selector: "betha-toolbar",
+  templateUrl: "./toolbar.component.html",
+  styleUrls: ["./toolbar.component.scss"],
 })
 export class ToolbarComponent {
+  criarCliente() {
+    const dialogRef = this.dialog.open(DialogCriarClienteComponent, {
+      maxWidth: "600px",
+    });
+    dialogRef.afterClosed().subscribe((result) => {});
+  }
   onCadastro() {
-    this.router.navigate(['cadastrar']);
+    this.router.navigate(["cadastrar"]);
   }
   onHome() {
-    this.router.navigate(['home']);
+    this.router.navigate(["home"]);
   }
   errorMessage: string =
-    'Ops! Esta funcionalidade ainda não está disponível. Estamos trabalhando nisso.';
-  constructor(private router: Router) {}
+    "Ops! Esta funcionalidade ainda não está disponível. Estamos trabalhando nisso.";
+  constructor(private router: Router, public dialog: MatDialog) {}
   onLogin() {
-    this.router.navigate(['login']);
+    this.router.navigate(["login"]);
   }
 }
