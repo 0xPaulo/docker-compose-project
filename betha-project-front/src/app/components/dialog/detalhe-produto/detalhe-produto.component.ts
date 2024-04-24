9beae66c30a6801ce1b756dcdb5e250bef56e853
0xPaulo <paulo_czar@outlook.com.br>
2024-04-24 19:41:18 -0300

    arrumando mostrar detalhes do tecnico para cada chamado

--- betha-project-front/src/app/components/dialog/detalhe-produto/detalhe-produto.component.ts
+++ betha-project-front/src/app/components/dialog/detalhe-produto/detalhe-produto.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { ImgProxyService } from "src/app/services/img-proxy.service";
 
 @Component({
   selector: "detalhe-produto",
@@ -10,8 +11,10 @@ import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 export class DetalheProdutoComponent {
   form: FormGroup;
   dia: string = "";
+  tecnicoImg!: string | ArrayBuffer;
 
   constructor(
+    private imgProxyService: ImgProxyService,
     private formBuilder: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
@@ -28,7 +31,24 @@ export class DetalheProdutoComponent {
       image_urls: [data.infoCadastro.image_urls],
       analiseTecnica: [data.infoCadastro.analiseTecnica],
       custoEstimado: [data.infoCadastro.custoEstimado],
+      tecnico: [data.infoCadastro.tecnico],
+      tecnicoImg: [data.infoCadastro.tecnicoImg],
+      // tecnicoNome: [data.infoCadastro.tecnicoNome],
+      // nao precisa de nada disso
+      tecnicoCategorias: [data.infoCadastro.tecnicoCategorias],
     });
     this.dia = data.infoCadastro.dataEntrada;
+    console.log(this.form.value);
+    if (data.infoCadastro.tecnico) {
+      this.imgProxyService
+        .getImage(this.data.infoCadastro.tecnicoImg)
+        .subscribe((blob) => {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            this.tecnicoImg = reader.result as string;
+          };
+          reader.readAsDataURL(blob);
+        });
+    }
   }
 }
