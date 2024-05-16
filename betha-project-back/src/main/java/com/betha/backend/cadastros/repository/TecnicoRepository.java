66980b428e7d8dbea0da6dc2a50033c3678e9e60
0xPaulo <paulo_czar@outlook.com.br>
2024-05-16 18:47:42 -0300

    implmentando spring security

--- betha-project-back/src/main/java/com/betha/backend/cadastros/repository/TecnicoRepository.java
+++ betha-project-back/src/main/java/com/betha/backend/cadastros/repository/TecnicoRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.betha.backend.cadastros.models.Tecnico;
 import com.betha.backend.cadastros.models.Enums.TecnicoCategorias;
@@ -13,4 +14,5 @@ public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
   @Query("SELECT t FROM Tecnico t WHERE t.tecnicoCategorias = :categoria")
   List<Tecnico> findByTecnicoCategorias(@Param("categoria") TecnicoCategorias categoria);
 
+  UserDetails findByEmail(String login);
 }
