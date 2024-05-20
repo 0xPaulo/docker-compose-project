3beed128a97dd76736afa29eafa1b04e1aae6cf5
0xPaulo <paulo_czar@outlook.com.br>
2024-05-20 16:29:58 -0300

    JWT tolken funcionando backend fix

--- betha-project-back/src/main/java/com/betha/backend/cadastros/controller/TecnicoController.java
+++ betha-project-back/src/main/java/com/betha/backend/cadastros/controller/TecnicoController.java
@@ -5,20 +5,25 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.betha.backend.cadastros.chamadoDTO.AutorizacaoDTO;
+import com.betha.backend.cadastros.chamadoDTO.AuthenticateDTO;
+import com.betha.backend.cadastros.chamadoDTO.LoginResponseDTO;
+import com.betha.backend.cadastros.chamadoDTO.RegisterDTO;
 import com.betha.backend.cadastros.models.Chamado;
 import com.betha.backend.cadastros.models.Tecnico;
 import com.betha.backend.cadastros.models.Enums.TecnicoCategorias;
 import com.betha.backend.cadastros.repository.TecnicoRepository;
 import com.betha.backend.cadastros.service.TecnicoService;
+import com.betha.backend.cadastros.service.TokenService;
 
 import jakarta.validation.Valid;
 
@@ -29,18 +34,32 @@ public class TecnicoController {
   private final TecnicoRepository tecnicoRepository;
   private final TecnicoService tecnicoService;
   private final AuthenticationManager authenticationManager;
+  private final TokenService tokenService;
 
   public TecnicoController(TecnicoRepository tecnicoRepository, TecnicoService tecnicoService,
-      AuthenticationManager authenticationManager) {
+      AuthenticationManager authenticationManager, TokenService tokenService) {
     this.tecnicoRepository = tecnicoRepository;
     this.tecnicoService = tecnicoService;
     this.authenticationManager = authenticationManager;
+    this.tokenService = tokenService;
   }
 
-  @GetMapping("/auth")
-  public ResponseEntity<?> login(@RequestBody @Valid AutorizacaoDTO dados) {
+  @GetMapping("/login")
+  public ResponseEntity<?> login(@RequestBody @Valid AuthenticateDTO dados) {
     var usernamePassword = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
     var auth = this.authenticationManager.authenticate(usernamePassword);
+
+    var token = tokenService.generateToken((Tecnico) auth.getPrincipal());
+    return ResponseEntity.ok(new LoginResponseDTO(token));
+  }
+
+  @PostMapping("/register")
+  public ResponseEntity register(@RequestBody @Valid RegisterDTO dados) {
+    if (this.tecnicoRepository.findByEmail(dados.email()) != null)
+      return ResponseEntity.badRequest().build();
+    String encryptPassword = new BCryptPasswordEncoder().encode(dados.senha());
+    Tecnico novoTecnico = new Tecnico(dados.email(), encryptPassword, dados.perfil());
+    this.tecnicoRepository.save(novoTecnico);
     return ResponseEntity.ok().build();
   }
 
@@ -62,7 +81,5 @@ public class TecnicoController {
     Chamado resultado = tecnicoService.editar(id, tecnicoID);
     return resultado;
   }
-
-  // criar tecnico
 
 }
---
+++ betha-project-back/src/main/java/com/betha/backend/cadastros/service/TokenService.java
@@ -0,0 +1,51 @@
+package com.betha.backend.cadastros.service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.betha.backend.cadastros.models.Tecnico;
+
+@Service
+public class TokenService {
+
+  // @Value("${api.security.secret}")
+  // private String secret;
+
+  public String generateToken(Tecnico tecnico) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256("secret");
+      String token = JWT.create()
+          .withIssuer("auth-api")
+          .withSubject(tecnico.getEmail())
+          .withExpiresAt(genExpirationDate())
+          .sign(algorithm);
+      return token;
+    } catch (JWTCreationException e) {
+      throw new RuntimeException("Erro while generation token", e);
+    }
+  }
+
+  public String validateToken(String token) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256("secret");
+      return JWT.require(algorithm)
+          .withIssuer("auth-api")
+          .build()
+          .verify(token)
+          .getSubject();
+    } catch (JWTVerificationException e) {
+      return "";
+    }
+  }
+
+  private Instant genExpirationDate() {
+    return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+  }
+}
