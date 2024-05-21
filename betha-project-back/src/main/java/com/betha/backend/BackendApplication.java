35cb44444135e009287dde1ddc8b975f55309b15
0xPaulo <paulo_czar@outlook.com.br>
2024-05-21 00:01:40 -0300

    variaveis de ambiente funcionando

--- betha-project-back/src/main/java/com/betha/backend/BackendApplication.java
+++ betha-project-back/src/main/java/com/betha/backend/BackendApplication.java
@@ -3,10 +3,20 @@ package com.betha.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		String DB_HOST = dotenv.get("DB_HOST");
+		String DB_PORT = dotenv.get("DB_PORT");
+
+		System.out.println("URL do banco de dados: " + DB_HOST);
+		System.out.println("PORTA do banco de dados: " + DB_PORT);
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
