3b03c24ffceec7aa0ef998bc7542d4ea16e2fa0e
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-05-27 16:56:53 -0300

    controle criado respeitar rotas get Manu/Tria/Conclu

---
+++ betha-project-back/src/main/java/com/betha/backend/cadastros/controller/authorizeRoutesController/TriagemController.java
@@ -0,0 +1,35 @@
+package com.betha.backend.cadastros.controller.authorizeRoutesController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.betha.backend.cadastros.chamadoDTO.ChamadoCompletoDTO;
+import com.betha.backend.cadastros.repository.ChamadoRepository;
+import com.betha.backend.cadastros.service.ChamadoService;
+
+@RestController
+@RequestMapping("/triagem")
+public class TriagemController {
+
+  @Autowired
+  private final ChamadoService chamadoService;
+  @Autowired
+  private final ChamadoRepository chamadoRepository;
+
+  TriagemController(ChamadoService chamadoService,
+      ChamadoRepository chamadoRepository) {
+    this.chamadoService = chamadoService;
+    this.chamadoRepository = chamadoRepository;
+  }
+
+  @GetMapping()
+  public List<ChamadoCompletoDTO> buscarTodosChamadosComFiltro(@RequestParam(required = false) List<String> params) {
+    List<ChamadoCompletoDTO> resultado = chamadoService.todosChamados(params);
+    return resultado;
+  }
+}
