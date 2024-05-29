0c5461dbd35a7bec691dc288d52405540861cbfe
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-05-29 16:48:15 -0300

    botao logout adicionado

--- betha-project-front/src/app/components/toolbar/toolbar.component.ts
+++ betha-project-front/src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { Router } from "@angular/router";
+import { AuthService } from "./../../services/auth.service";
 import { DialogCriarClienteComponent } from "./dialog-criar-cliente/dialog-criar-cliente.component";
 
 @Component({
@@ -9,6 +10,12 @@ import { DialogCriarClienteComponent } from "./dialog-criar-cliente/dialog-criar
   styleUrls: ["./toolbar.component.scss"],
 })
 export class ToolbarComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    public dialog: MatDialog
+  ) {}
+
   criarCliente() {
     const dialogRef = this.dialog.open(DialogCriarClienteComponent, {
       maxWidth: "600px",
@@ -26,8 +33,22 @@ export class ToolbarComponent {
   }
   errorMessage: string =
     "Ops! Esta funcionalidade ainda não está disponível. Estamos trabalhando nisso.";
-  constructor(private router: Router, public dialog: MatDialog) {}
+
   onLogin() {
     this.router.navigate(["login"]);
   }
+
+  sair() {
+    this.authService.logout();
+    this.router.navigate(["/login"]);
+  }
+
+  // canActivate(): boolean {
+  //   if (this.authService.isLoggedIn()) {
+  //     return true;
+  //   } else {
+  //     this.router.navigate(["/login"]);
+  //     return false;
+  //   }
+  // }
 }
