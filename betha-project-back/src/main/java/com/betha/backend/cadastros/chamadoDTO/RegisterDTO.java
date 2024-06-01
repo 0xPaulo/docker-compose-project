4d88a25e285e00cd5373cc738417924833d56a53
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-06-01 18:00:30 -0300

    atualizando record add mais parametros

--- betha-project-back/src/main/java/com/betha/backend/cadastros/chamadoDTO/RegisterDTO.java
+++ betha-project-back/src/main/java/com/betha/backend/cadastros/chamadoDTO/RegisterDTO.java
@@ -1,7 +1,9 @@
 package com.betha.backend.cadastros.chamadoDTO;
 
 import com.betha.backend.cadastros.models.Enums.Perfils;
+import com.betha.backend.cadastros.models.Enums.TecnicoCategorias;
 
-public record RegisterDTO(String email, String senha, Perfils perfil) {
+public record RegisterDTO(String id, String email, String senha, Perfils perfil, String nome,
+    TecnicoCategorias tecnicoCategorias) {
 
 }
