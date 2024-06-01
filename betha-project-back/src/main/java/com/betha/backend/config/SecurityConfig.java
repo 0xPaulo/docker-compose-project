8b13511db9ca983e43c8684ca0a5155afd322cf4
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-06-01 18:04:34 -0300

    arrumando identacao da calsse e add rota de password-patch

--- betha-project-back/src/main/java/com/betha/backend/config/SecurityConfig.java
+++ betha-project-back/src/main/java/com/betha/backend/config/SecurityConfig.java
@@ -17,64 +17,83 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Autowired
-    SecurityFilter securityFilter;
+  @Autowired
+  SecurityFilter securityFilter;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(
-                        authorize -> authorize
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            authorize -> authorize
 
-                                .requestMatchers(HttpMethod.POST, "/tecnico/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/tecnico/login")
+                .permitAll()
 
-                                // Cadastrar
-                                .requestMatchers(HttpMethod.GET, "/cadastros").hasAnyRole("ADMIN",
-                                        "RECEPCAO")
-                                .requestMatchers(HttpMethod.PATCH, "/cadastros").hasAnyRole("ADMIN",
-                                        "RECEPCAO")
-                                .requestMatchers(HttpMethod.POST, "/cadastros").hasAnyRole("ADMIN",
-                                        "RECEPCAO")
-                                .requestMatchers(HttpMethod.DELETE, "/cadastros").hasAnyRole("ADMIN",
-                                        "RECEPCAO")
-                                .requestMatchers(HttpMethod.POST, "/clientes").hasAnyRole("ADMIN",
-                                        "RECEPCAO")
+                // Cadastrar
+                .requestMatchers(HttpMethod.GET, "/cadastros")
+                .hasAnyRole("ADMIN",
+                    "RECEPCAO")
+                .requestMatchers(HttpMethod.PATCH, "/cadastros")
+                .hasAnyRole("ADMIN",
+                    "RECEPCAO")
+                .requestMatchers(HttpMethod.POST, "/cadastros")
+                .hasAnyRole("ADMIN",
+                    "RECEPCAO")
+                .requestMatchers(HttpMethod.DELETE, "/cadastros")
+                .hasAnyRole("ADMIN",
+                    "RECEPCAO")
+                .requestMatchers(HttpMethod.POST, "/clientes")
+                .hasAnyRole("ADMIN",
+                    "RECEPCAO")
 
-                                // Triagem
-                                .requestMatchers(HttpMethod.GET, "/triagem").hasAnyRole("ADMIN", "TRIAGEM")
-                                .requestMatchers(HttpMethod.PATCH, "/triagem").hasAnyRole("ADMIN", "TRIAGEM")
+                // Triagem
+                .requestMatchers(HttpMethod.GET, "/triagem")
+                .hasAnyRole("ADMIN", "TRIAGEM")
+                .requestMatchers(HttpMethod.PATCH, "/triagem")
+                .hasAnyRole("ADMIN", "TRIAGEM")
 
-                                // Tecnico
-                                .requestMatchers(HttpMethod.GET, "/tecnico").hasAnyRole("ADMIN", "TECNICO")
-                                .requestMatchers(HttpMethod.POST, "/tecnico").hasAnyRole("ADMIN", "TECNICO")
-                                .requestMatchers(HttpMethod.PATCH, "/tecnico").hasAnyRole("ADMIN", "TECNICO")
-                                .requestMatchers(HttpMethod.POST, "/uploadToGoogleDrive").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/proxy").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/concluido").hasAnyRole("ADMIN", "TECNICO")
+                // Tecnico
+                .requestMatchers(HttpMethod.GET, "/tecnico")
+                .hasAnyRole("ADMIN", "TECNICO")
+                .requestMatchers(HttpMethod.POST, "/tecnico")
+                .hasAnyRole("ADMIN", "TECNICO")
+                .requestMatchers(HttpMethod.PATCH, "/tecnico")
+                .hasAnyRole("ADMIN", "TECNICO")
+                .requestMatchers(HttpMethod.POST,
+                    "/uploadToGoogleDrive")
+                .permitAll()
+                .requestMatchers(HttpMethod.GET, "/proxy").permitAll()
+                .requestMatchers(HttpMethod.GET, "/concluido")
+                .hasAnyRole("ADMIN", "TECNICO", "RECEPCAO")
 
-                                // Manutencao
-                                .requestMatchers(HttpMethod.GET, "/tecnico/chamados").hasAnyRole("ADMIN",
-                                        "MANUTENCAO")
-                                .requestMatchers(HttpMethod.PATCH, "/cadastros/change-status").hasAnyRole("ADMIN",
-                                        "MANUTENCAO")
+                // Manutencao
+                .requestMatchers(HttpMethod.GET, "/tecnico/chamados")
+                .hasAnyRole("ADMIN", "MANUTENCAO")
+                .requestMatchers(HttpMethod.PATCH,
+                    "/cadastros/change-status")
+                .hasAnyRole("ADMIN", "MANUTENCAO")
 
-                                // ADMIN
-                                .requestMatchers(HttpMethod.POST, "/tecnico/register").hasRole("ADMIN")
-                                .anyRequest().authenticated())
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
-    }
+                // ADMIN
+                .requestMatchers(HttpMethod.POST, "/tecnico/register")
+                .hasAnyRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/tecnico/password-patch")
+                .hasAnyRole("ADMIN")
+                .anyRequest().authenticated())
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
+  }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+      throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
