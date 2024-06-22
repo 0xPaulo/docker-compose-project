3622563647893b959aff540b05e8e5bcf8698986
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-06-22 18:58:53 -0300

    primeiro test passando

---
+++ betha-project-back/src/test/java/com/betha/backend/cadastros/service/TecnicoServiceTest.java
@@ -0,0 +1,51 @@
+package com.betha.backend.cadastros.service;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.betha.backend.cadastros.models.Chamado;
+import com.betha.backend.cadastros.models.Tecnico;
+import com.betha.backend.cadastros.repository.ChamadoRepository;
+import com.betha.backend.cadastros.repository.TecnicoRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class TecnicoServiceTest {
+
+	@InjectMocks
+	TecnicoService tecnicoService;
+
+	@Mock
+	ChamadoRepository chamadoRepository;
+
+	@Mock
+	TecnicoRepository tecnicoRepository;
+
+	@Test
+	@DisplayName("Deve setar um novo tecnico ao chamado")
+	public void shouldEditarTecnicoDoChamadoSucesso() {
+
+		// arrange
+		Long chamadoId = (long) 1;
+		Long tecnicoId = (long) 1;
+
+		Chamado novoChamado = new Chamado();
+		Tecnico novoTecnico = new Tecnico();
+
+		Mockito.when(chamadoRepository.findById(chamadoId)).thenReturn(Optional.of(novoChamado));
+
+		Mockito.when(tecnicoRepository.findById(tecnicoId)).thenReturn(Optional.of(novoTecnico));
+
+		// action
+		tecnicoService.editarTecnicoDoChamado(chamadoId, tecnicoId);
+
+		// assertions
+	}
+
+}
