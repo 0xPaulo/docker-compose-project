4f1ab1921d45f55434b92da7244925907a40d170
Paulo <102301140+0xPaulo@users.noreply.github.com>
2024-07-01 17:40:38 -0300

    criando metodo e construtor para facilitar os testes

--- betha-project-back/src/main/java/com/betha/backend/cadastros/chamadoDTO/FormCliente.java
+++ betha-project-back/src/main/java/com/betha/backend/cadastros/chamadoDTO/FormCliente.java
@@ -1,5 +1,7 @@
 package com.betha.backend.cadastros.chamadoDTO;
 
+import java.lang.reflect.Field;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +14,25 @@ public class FormCliente {
   private String clienteEmail;
   private String clienteTelefone;
   private String clienteEndereco;
+
   // private String imagem;
   // private Perfils perfil;
+  public Object getValorDoCampo(String campo) {
+    try {
+      Field field = getClass().getDeclaredField(campo);
+      field.setAccessible(true);
+      return field.get(this);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new RuntimeException("Erro ao acessar campo " + campo, e);
+    }
+  }
+
+  public FormCliente(long clienteId, String clienteNome, String clienteEmail, String clienteTelefone,
+      String clienteEndereco) {
+    this.clienteId = clienteId;
+    this.clienteNome = clienteNome;
+    this.clienteEmail = clienteEmail;
+    this.clienteTelefone = clienteTelefone;
+    this.clienteEndereco = clienteEndereco;
+  }
 }
